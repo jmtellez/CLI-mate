eb37b2ec21ea4c2e29d3344b045d873af0666f2a
abhishek bhaware <64492784+cosmoimai@users.noreply.github.com>
2020-10-06 04:01:01 +0530

    TOKEN MISTAKE

--- utils/geocode.js
+++ utils/geocode.js
@@ -4,11 +4,10 @@ const ora = require("ora");
 const geocode = (address, callback) => {
   const geocodingURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
     address
-  )}.json?access_token=pk.eyJ1IjoiYWJoaXNoZWtiaGF3YXJlIiwiYSI6ImNrYnF2dzBicjAxYjcycXF4emQzcG9lcm4ifQ.TCBvoJc65X2vVdgPz_lKag&limit=1`;
+  )}.json?access_token=${process.env.MAPBOX}`;
   const spinner = ora("Getting geocodes").start();
   setTimeout(() => {
     request({ url: geocodingURL, json: true }, (err, { body } = {}) => {
-      //console.log("ashu " + body.features.length);
       if (err) {
         spinner.stop();
         callback("Unable to connect to mapbox services", undefined);
