7c5d838ac73cef494caee65640d7cdda0a815486
arpitvaghela <arpitvaghela9210@gmail.com>
2020-10-09 10:03:38 +0530

    resolved #20 added error handling for API keys

--- app.js
+++ app.js
@@ -11,50 +11,57 @@ const args = process.argv;
 let units;
 let location;
 
-if (args[2] === "-h" || args[2] === "--help") {
-  menu();
-  return;
-}
-if (args[2] === "-v" || args[2] === "--version") {
-  spinner.succeed(pck.version);
-  return;
-}
-
-for (let i = 2; i < args.length; i++) {
-  if (args[i].startsWith("--u")) {
-    parseunit = args[i].split("=")[1];
-    if (['m', 's', 'f'].includes(parseunit)) {
-      units = parseunit;
-    }
+(function(){
+  if(!process.env.WEATHERSTACK || !process.env.MAPBOX){
+    spinner.fail("Failed to get API keys");
+    return;
   }
-  else if (!location) {
-    location = args[i];
+  if (args[2] === "-h" || args[2] === "--help") {
+    menu();
+    return;
   }
-}
-
-if (!location) {
-  menu();
-  return spinner.fail("Provide a location");
-}
-
-geocode(location, (err, { latitude, longitude, location } = {}) => {
-  if (err) {
-    return spinner.fail(err);
+  if (args[2] === "-v" || args[2] === "--version") {
+    spinner.succeed(pck.version);
+    return;
   }
-  forecast(
-    latitude,
-    longitude,
-    units,
-    (err, { description, temp, feelsLike, tempScale } = {}) => {
-      if (err) {
-        return spinner.fail(err);
+
+  for (let i = 2; i < args.length; i++) {
+    if (args[i].startsWith("--u")) {
+      parseunit = args[i].split("=")[1];
+      if (['m', 's', 'f'].includes(parseunit)) {
+        units = parseunit;
       }
-      spinner.succeed(chalk.underline(location));
-      console.log(
-        chalk.cyanBright(
-          `${description}. It is currently ${temp}${tempScale}, it feels like ${feelsLike}${tempScale}.`
-        )
-      );
     }
-  );
-});
+    else if (!location) {
+      location = args[i];
+    }
+  }
+
+  if (!location) {
+    menu();
+    return spinner.fail("Provide a location");
+  }
+
+  geocode(location, (err, { latitude, longitude, location } = {}) => {
+    if (err) {
+      return spinner.fail(err);
+    }
+    forecast(
+      latitude,
+      longitude,
+      units,
+      (err, { description, temp, feelsLike, tempScale } = {}) => {
+        if (err) {
+          return spinner.fail(err);
+        }
+        spinner.succeed(chalk.underline(location));
+        console.log(
+          chalk.cyanBright(
+            `${description}. It is currently ${temp}${tempScale}, it feels like ${feelsLike}${tempScale}.`
+          )
+        );
+      }
+    );
+  });
+  
+})();
